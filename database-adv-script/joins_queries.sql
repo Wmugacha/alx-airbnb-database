208217e33ba136b9624f856621bfece7dd904b77
Wmugacha <wilfredmugacha@gmail.com>
2025-05-13 10:02:35 +0800

    Inner join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,14 @@
-SELECT first_name, last_name, booking_id, property_id
+SELECT first_name.Users, last_name.Users, booking_id.Bookings, property_id.Bookings
 FROM Users
 INNER JOIN Bookings
+ON Users.user_id = Bookings.user_id
+
+SELECT name.Properties, comment.Reviews, rating.Reviews
+FROM Properties
+LEFT JOIN Reviews
+ON Properties.property_id = Reviews.property_id
+
+SELECT first_name.Users, last_name.Users, booking_id.Bookings, property_id.Bookings
+FROM Users
+FULL OUTER JOIN Bookings
 ON Users.user_id = Bookings.user_id
