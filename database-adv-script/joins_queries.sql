7b6bc6a8b43b9eb40778649ad402a7b225dc4319
Wmugacha <wilfredmugacha@gmail.com>
2025-05-13 10:06:08 +0800

    Inner join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,6 +7,7 @@ SELECT name.Properties, comment.Reviews, rating.Reviews
 FROM Properties
 LEFT JOIN Reviews
 ON Properties.property_id = Reviews.property_id
+ORDER BY name.Properties
 
 SELECT first_name.Users, last_name.Users, booking_id.Bookings, property_id.Bookings
 FROM Users
