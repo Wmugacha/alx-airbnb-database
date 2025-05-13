39b1712da3cef1f566a0ac839e49c5d396fadcd9
Wmugacha <wilfredmugacha@gmail.com>
2025-05-13 11:11:15 +0800

    subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,7 +1,17 @@
 SELECT property_name
 FROM Properties
-WHERE property_id IN (SELECT property_id FROM Reviews WHERE AVG(rating) > 4.0);
+WHERE property_id IN (
+    SELECT property_id
+    FROM Reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
 SELECT first_name, last_name
 FROM Users
-WHERE user_id IN (SELECT user_id FROM Bookings WHERE COUNT(booking_id) > 3);
+WHERE user_id IN (
+    SELECT user_id
+    FROM Bookings
+    GROUP BY user_id
+    HAVING COUNT(booking_id) > 3
+);
