3fbe24e1e690949c254ff72062e96e4c16e9ba9c
Wmugacha <wilfredmugacha@gmail.com>
2025-05-16 16:09:16 +0800

    monitor database perfomance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,8 @@
+EXPLAIN ANALYZE SELECT COUNT(*) AS host_count
+FROM users
+JOIN roles on users.role_id = roles.role_id
+WHERE roles.role_name = 'host';
+
 CREATE INDEX idx_users_id ON Users (user_id);
 
 CREATE INDEX idx_property_id ON Properties(property_id);
@@ -5,3 +10,10 @@ CREATE INDEX idx_property_id ON Properties(property_id);
 CREATE INDEX idx_booking_id ON Bookings(booking_id)
 
 CREATE idx_role_id ON Roles(role_id)
+
+EXPLAIN ANALYZE
+SELECT COUNT(*) AS host_count
+FROM users
+JOIN roles
+ON users.role_id = roles.role_id
+WHERE roles.role_name = 'host';
